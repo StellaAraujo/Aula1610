aaccc04fbd773eb14ac84aa44ec2a57a73393307
felipinaseosd <130668980+felipinaseosd@users.noreply.github.com>
2023-10-16 09:48:16 -0300

    Primeiro commit

---
+++ Desafios/Aula8/Ex1-par_impar.py
@@ -0,0 +1,15 @@
+#Apresentação
+print('\n\t\t\t -- Verificador de Núm. Pares e Ímpares')
+
+#Entrada
+num = int(input('Digite um número: '))
+
+#Processamento e saída
+if num % 2 == 0:
+    print(f'{num} é par')
+else:
+    print(f'{num} é ímpar')
+
+
+
+
